68c5d2318e181b5c09ce4af89c552bc387fd3237
Álvaro Peña <alvaropg@gmail.com>
2019-09-23 15:53:21 +0200

    Initial commit with base code to test the FreeType2 initialize process

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((c-mode
+  (flycheck-clang-include-path "/usr/include/freetype2"
+                               "/usr/include/libpng16"
+                               "/usr/include/glib-2.0"
+                               "/usr/lib64/glib-2.0/include")))
---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+        clock_t t;
+        double time_taken;
+        unsigned int measure_num = 0;
+        int error;
+        FT_Library library;
+        FT_Face face;
+
+        t = clock();
+        error = FT_Init_FreeType(&library);
+        if (error != FT_Err_Ok) {
+                printf("Error initializing FreeType library\n");
+                return error;
+        }
+        t = clock() - t;
+        time_taken = ((double) t) / CLOCKS_PER_SEC;
+        printf("%d Took %f seconds\n", measure_num++, time_taken);
+
+        t = clock();
+        error = FT_New_Face(library,
+                            "/home/alvaropg/work/tmp/freetype/fonts/FiraSans/FiraSans-Medium.ttf",
+                            0,
+                            &face);
+        if (error == FT_Err_Unknown_File_Format) {
+                printf("Unknown file format\n");
+                return error;
+        } else if (error != FT_Err_Ok) {
+                printf("Error loading font: %d\n", error);
+                return error;
+        }
+        t = clock() - t;
+        time_taken = ((double) t) / CLOCKS_PER_SEC;
+        printf("%d Took %f seconds\n", measure_num++, time_taken);        
+
+        printf("Font info: Glyphs: %ld\n", face->num_glyphs);
+
+        return 0;
+}
